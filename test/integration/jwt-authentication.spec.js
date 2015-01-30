9f9cd6d99f32699727268dd1729f99a14e7e39b7
Ben Sayers <bsayers@atlassian.com>
2015-01-30 14:12:49 +1100

    test: Update the way generateToken and generateAuthorizationHeader share tests so they fail independently of each other

--- test/integration/jwt-authentication.spec.js
+++ test/integration/jwt-authentication.spec.js
@@ -5,106 +5,102 @@ var _ = require('lodash');
 
 describe ('jwt-authentication', function () {
 
-    // extract 'token' from 'x-atl-jwt token'
-    var getTokenFromAuthHeader = function(header) {
-        return header.split(' ')[1];
-    };
+    var privateKey = fs.readFileSync('test/integration/key-server/an-issuer/private.pem');
+    var incorrectPrivateKey = fs.readFileSync('test/integration/key-server/an-issuer/private-wrong.pem');
 
-    var createTokenCreator = function() {
-        return jwtAuthentication.create({publicKeyServer: 'public-key-server-url'});
-    };
-
-    var validateJwtToken = function (token, publicKeyName) {
-        var publicKey = fs.readFileSync('test/integration/key-server/an-issuer/' + publicKeyName + '.pem');
-        return jwtSimple.decode(token, publicKey);
+    var createAuthenticator = function (publicKeyServer) {
+        return jwtAuthentication.create({publicKeyServer: publicKeyServer || 'http://localhost:8000'});
     };
 
     it('should throw error if config is null', function() {
-        expect(function() {jwtAuthentication.create(null);}).toThrow(
-            new Error('Required config value config.publicKeyServer is missing.'));
+        var create = _.partial(jwtAuthentication.create, null);
+        expect(create).toThrow(new Error('Required config value config.publicKeyServer is missing.'));
     });
 
     it('should throw error if config is missing publicKeyServer field', function() {
-        expect(function() {jwtAuthentication.create({foo: 'bar'});}).toThrow(
-            new Error('Required config value config.publicKeyServer is missing.'));
+        var create = _.partial(jwtAuthentication.create, {foo: 'bar'});
+        expect(create).toThrow(new Error('Required config value config.publicKeyServer is missing.'));
     });
 
-
-    describe('generateToken and generateAuthorizationHeader', function () {
-        var shouldCreateCorrectlySignedToken = function(functionToTest, extractTokenFunction, done) {
-            var privateKey = fs.readFileSync('test/integration/key-server/an-issuer/private.pem');
-            createTokenCreator()[functionToTest]({iss: 'an-issuer', sub: 'a-subject', foo: 'abc', bar: 123},
-                {privateKey: privateKey}, function (error, data) {
-                    expect(error).toBeNull('error');
-                    var token = extractTokenFunction(data);
-                    var decodedJwt = validateJwtToken(token, 'public');
-
-                    expect(decodedJwt.iss).toBe('an-issuer');
-                    expect(decodedJwt.sub).toBe('a-subject');
-                    expect(decodedJwt.foo).toBe('abc');
-                    expect(decodedJwt.bar).toBe(123);
-                    done();
-                });
+    var itShouldGenerateAToken = function (generateToken) {
+        var validateJwtToken = function (token, publicKeyName) {
+            var publicKey = fs.readFileSync('test/integration/key-server/an-issuer/' + publicKeyName + '.pem');
+            return jwtSimple.decode(token, publicKey);
         };
 
         it('should create a correctly signed jwt token', function (done) {
-            shouldCreateCorrectlySignedToken('generateToken', _.identity, done);
-            shouldCreateCorrectlySignedToken('generateAuthorizationHeader', getTokenFromAuthHeader, done);
+            var claims = {iss: 'an-issuer', sub: 'a-subject', foo: 'abc', bar: 123};
+            generateToken(claims, {privateKey: privateKey}, function (error, token) {
+                expect(error).toBeNull('error');
+
+                var actualClaims = validateJwtToken(token, 'public');
+                expect(actualClaims.iss).toBe('an-issuer');
+                expect(actualClaims.sub).toBe('a-subject');
+                expect(actualClaims.foo).toBe('abc');
+                expect(actualClaims.bar).toBe(123);
+                done();
+            });
         });
 
-        var verifactionShouldFailIfTokenSignedWithWrongKey = function(functionToTest, extractTokenFunction, done) {
-            var privateKey = fs.readFileSync('test/integration/key-server/an-issuer/private-wrong.pem');
-            createTokenCreator()[functionToTest]({iss: 'an-issuer', sub: 'a-subject'}, {privateKey: privateKey},
-                function(error, data) {
-                    expect(error).toBeNull('error');
-                    var token = extractTokenFunction(data);
-                    expect( function() {validateJwtToken(token, 'public');}).toThrow(
-                        new Error('Signature verification failed'));
-                    done();
-                });
-        };
 
         it('should create a signed jwt token that can only be verified with the right public key', function (done) {
-            verifactionShouldFailIfTokenSignedWithWrongKey('generateToken', _.identity, done);
-            verifactionShouldFailIfTokenSignedWithWrongKey('generateAuthorizationHeader', getTokenFromAuthHeader, done);
-        });
+            var claims = {iss: 'an-issuer', sub: 'a-subject'};
+            generateToken(claims, {privateKey: incorrectPrivateKey}, function(error, token) {
+                expect(error).toBeNull('error');
 
-        var shouldFailIfClaimsIsMissingIssField = function (functionToTest, done) {
-            var privateKey = fs.readFileSync('test/integration/key-server/an-issuer/private.pem');
-            createTokenCreator()[functionToTest]({sub: 'a-subject'}, {privateKey: privateKey},
-                function(error, token) {
-                    expect(error).toEqual(new Error('claims body must have both the "iss" and "sub" fields'));
-                    expect(token).toBeUndefined();
-                    done();
-                });
-        };
+                var validate = _.partial(validateJwtToken, token, 'public');
+                expect(validate).toThrow(new Error('Signature verification failed'));
+                done();
+            });
+        });
 
         it('should return an error if claims are missing "iss" field', function (done) {
-            shouldFailIfClaimsIsMissingIssField('generateToken', done);
-            shouldFailIfClaimsIsMissingIssField('generateAuthorizationHeader', done);
+            generateToken({sub: 'a-subject'}, {privateKey: privateKey}, function (error, token) {
+                expect(error).toEqual(new Error('claims body must have both the "iss" and "sub" fields'));
+                expect(token).toBeUndefined();
+                done();
+            });
         });
-
-        var shouldFailIfClaimsIsMissingSubField = function (functionToTest, done) {
-            var privateKey = fs.readFileSync('test/integration/key-server/an-issuer/private.pem');
-            createTokenCreator()[functionToTest]({iss: 'an-issuer'}, {privateKey: privateKey},
-                function(error, token) {
-                    expect(error).toEqual(new Error('claims body must have both the "iss" and "sub" fields'));
-                    expect(token).toBeUndefined();
-                    done();
-                });
-        };
 
         it('should return an error if claims are missing "sub" field', function (done) {
-            shouldFailIfClaimsIsMissingSubField('generateToken', done);
-            shouldFailIfClaimsIsMissingSubField('generateAuthorizationHeader', done);
+            generateToken({iss: 'an-issuer'}, {privateKey: privateKey}, function (error, token) {
+                expect(error).toEqual(new Error('claims body must have both the "iss" and "sub" fields'));
+                expect(token).toBeUndefined();
+                done();
+            });
         });
+    };
+
+    describe('generateToken', function () {
+        var invokeGenerateToken = function (claims, options, callback) {
+            return createAuthenticator().generateToken(claims, options, callback);
+        };
+
+        itShouldGenerateAToken(invokeGenerateToken);
+    });
+
+    describe('generateAuthorizationHeader', function () {
+        it('should prefix the token with "x-atl-jwt" followed by a space', function (done) {
+            var claims = {iss: 'an-issuer', sub: 'a-subject'};
+            var options = {privateKey: privateKey};
+            createAuthenticator().generateAuthorizationHeader(claims, options, function (error, headerValue) {
+                expect(error).toBeNull('error');
+                expect(headerValue.substr(0, 10)).toBe('x-atl-jwt ');
+                done();
+            });
+        });
+
+        var invokeGenerateAuthorizationHeader = function (claims, options, callback) {
+            return createAuthenticator().generateAuthorizationHeader(claims, options, function (error, headerValue) {
+                var token = (headerValue || '').split(' ')[1];
+                callback(error, token);
+            });
+        };
+
+        itShouldGenerateAToken(invokeGenerateAuthorizationHeader);
     });
 
     describe('validate', function () {
-        var createValidator = function (publicKeyServer) {
-            return jwtAuthentication.create({publicKeyServer: publicKeyServer || 'http://localhost:8000'});
-        };
-
         var createJwtToken = function (privateKeyName, payload) {
             var privateKey = fs.readFileSync('test/integration/key-server/an-issuer/' + privateKeyName + '.pem');
             payload = payload || {'iss': 'an-issuer'};
@@ -114,7 +110,7 @@ describe ('jwt-authentication', function () {
         it('should validate a jwt token', function (done) {
             var jwtToken = createJwtToken('private');
 
-            createValidator().validate(jwtToken, function(error, claims) {
+            createAuthenticator().validate(jwtToken, function(error, claims) {
                 expect(error).toBeNull('error');
                 expect(claims).toEqual({iss: 'an-issuer'}, 'claims');
                 done();
@@ -124,7 +120,7 @@ describe ('jwt-authentication', function () {
         it('should return the error when unable to retrieve the public key', function (done) {
             var jwtToken = createJwtToken('private');
 
-            createValidator('http://localhost:8000/does-not-exist').validate(jwtToken, function (error, claims) {
+            createAuthenticator('http://localhost:8000/does-not-exist').validate(jwtToken, function (error, claims) {
                 expect(claims).toBeUndefined('claims');
                 expect(error).toBeDefined('error');
                 expect(error.message).toBe(
@@ -140,7 +136,7 @@ describe ('jwt-authentication', function () {
         it('should return the error when validating the jwt token fails', function (done) {
             var jwtToken = createJwtToken('private-wrong');
 
-            createValidator().validate(jwtToken, function(error, claims) {
+            createAuthenticator().validate(jwtToken, function(error, claims) {
                 expect(claims).toBeUndefined('claims');
                 expect(error).toBeDefined('error');
                 expect(error.message).toBe('invalid signature');
@@ -152,7 +148,7 @@ describe ('jwt-authentication', function () {
             var expiryTimeInSeconds = new Date(2013, 0, 1).getTime() / 1000;
             var jwtToken = createJwtToken('private', {iss: 'an-issuer', exp: expiryTimeInSeconds});
 
-            createValidator().validate(jwtToken, function(error, claims) {
+            createAuthenticator().validate(jwtToken, function(error, claims) {
                 expect(claims).toBeUndefined('claims');
                 expect(error).toBeDefined('error');
                 expect(error.message).toBe('jwt expired');
--- test/unit/jwt-authentication.spec.js
+++ test/unit/jwt-authentication.spec.js
@@ -11,7 +11,7 @@ describe('jwt-authentication', function () {
         jsonWebToken = jasmine.createSpyObj('jsonWebToken', ['create', 'decode', 'verify']);
         jsonWebToken.decode.andReturn({iss: 'default-issuer'});
         jsonWebToken.verify.andReturn(q());
-        jsonWebToken.create.andReturn('token');
+        jsonWebToken.create.andReturn('');
 
         request = jasmine.createSpy('request');
         request.andReturn(q());
@@ -31,179 +31,185 @@ describe('jwt-authentication', function () {
             new Error('Required config value config.publicKeyServer is missing.'));
     });
 
-    var checkGenerateTokenHappyPath = function(functionToTest, expectedTokenValue, done) {
-        validator[functionToTest]({iss: 'iss', sub: 'sub', foo: 'bar'}, {privateKey: 'key'}, function(error, token) {
-            expect(jsonWebToken.create).toHaveBeenCalledWith({iss: 'iss', sub: 'sub', foo: 'bar'}, 'key');
-            expect(error).toBeNull();
-            expect(token).toBe(expectedTokenValue);
-            done();
+    var itShouldGenerateAToken = function (methodToTestName) {
+        var generateToken = function (claims, options, callback) {
+            return validator[methodToTestName](claims, options, callback);
+        };
+
+        it('should pass arguments to create', function (done) {
+            generateToken({iss: 'iss', sub: 'sub', foo: 'bar'}, {privateKey: 'key'}, function () {
+                expect(jsonWebToken.create).toHaveBeenCalledWith({iss: 'iss', sub: 'sub', foo: 'bar'}, 'key');
+                done();
+            });
+        });
+
+        it('should throw an error if options is null', function (done) {
+            generateToken({iss: 'iss', sub: 'sub'}, null, function(error, token) {
+                expect(jsonWebToken.create).not.toHaveBeenCalled();
+                expect(error).toEqual(new Error('Required value options.privateKey is'));
+                expect(token).toBeUndefined();
+                done();
+            });
+        });
+
+        it('should throw an error if options.privateKey is missing', function (done) {
+            generateToken({iss: 'iss', sub: 'sub'}, {publicKey: 'key'}, function(error, token) {
+                expect(jsonWebToken.create).not.toHaveBeenCalled();
+                expect(error).toEqual(new Error('Required value options.privateKey is missing'));
+                expect(token).toBeUndefined();
+                done();
+            });
+        });
+
+        it('should throw an error if claims is null', function (done) {
+            generateToken(null, {publicKey: 'key'}, function(error, token) {
+                expect(jsonWebToken.create).not.toHaveBeenCalled();
+                expect(error).toEqual(new Error('claims body must have both the "iss" and "sub" fields'));
+                expect(token).toBeUndefined();
+                done();
+            });
+        });
+
+        it('should throw an error if "iss" field is missing from claims', function (done) {
+            generateToken({sub: 'sub'}, {publicKey: 'key'}, function(error, token) {
+                expect(jsonWebToken.create).not.toHaveBeenCalled();
+                expect(error).toEqual(new Error('claims body must have both the "iss" and "sub" fields'));
+                expect(token).toBeUndefined();
+                done();
+            });
+        });
+
+        it('should throw an error if "sub" field is missing from claims', function (done) {
+            generateToken({iss: 'iss'}, {publicKey: 'key'}, function(error, token) {
+                expect(jsonWebToken.create).not.toHaveBeenCalled();
+                expect(error).toEqual(new Error('claims body must have both the "iss" and "sub" fields'));
+                expect(token).toBeUndefined();
+                done();
+            });
         });
     };
 
-    it('should pass arguments to create', function(done) {
-        checkGenerateTokenHappyPath('generateToken', 'token', done);
-        checkGenerateTokenHappyPath('generateAuthorizationHeader', 'x-atl-jwt token', done);
-    });
+    describe('generateToken', function () {
+        it('should return the generated token', function (done) {
+            jsonWebToken.create.andReturn('token');
 
-    var checkThatErrorIsReturnedWhenOptionsIsNull = function(functionToTest, done) {
-        validator[functionToTest]({iss: 'iss', sub: 'sub'}, null, function(error, token) {
-            expect(jsonWebToken.create).not.toHaveBeenCalled();
-            expect(error).toEqual(new Error('Required value options.privateKey is'));
-            expect(token).toBeUndefined();
-            done();
-        });
-    };
-
-    it('should throw an error if options is null', function(done) {
-        checkThatErrorIsReturnedWhenOptionsIsNull('generateToken', done);
-        checkThatErrorIsReturnedWhenOptionsIsNull('generateAuthorizationHeader', done);
-    });
-
-    var checkThatErrorIsReturnedWhenPrivateKeyIsMissing = function(functionToTest, done) {
-        validator[functionToTest]({iss: 'iss', sub: 'sub'}, {publicKey: 'key'}, function(error, token) {
-            expect(jsonWebToken.create).not.toHaveBeenCalled();
-            expect(error).toEqual(new Error('Required value options.privateKey is missing'));
-            expect(token).toBeUndefined();
-            done();
-        });
-    };
-
-    it('should throw an error if options.privateKey is missing', function(done) {
-        checkThatErrorIsReturnedWhenPrivateKeyIsMissing('generateToken', done);
-        checkThatErrorIsReturnedWhenPrivateKeyIsMissing('generateAuthorizationHeader', done);
-    });
-
-    var checkThatErrorIsReturnedWhenClaimsIsNull = function(functionToTest, done) {
-        validator[functionToTest](null, {publicKey: 'key'}, function(error, token) {
-            expect(jsonWebToken.create).not.toHaveBeenCalled();
-            expect(error).toEqual(new Error('claims body must have both the "iss" and "sub" fields'));
-            expect(token).toBeUndefined();
-            done();
-        });
-    };
-
-    it('should throw an error if claims is null', function(done) {
-        checkThatErrorIsReturnedWhenClaimsIsNull('generateToken', done);
-        checkThatErrorIsReturnedWhenClaimsIsNull('generateAuthorizationHeader', done);
-    });
-
-    var checkThatErrorIsReturnedWhenClaimsHasNoIssField = function(functionToTest, done) {
-        validator[functionToTest]({sub: 'sub'}, {publicKey: 'key'}, function(error, token) {
-            expect(jsonWebToken.create).not.toHaveBeenCalled();
-            expect(error).toEqual(new Error('claims body must have both the "iss" and "sub" fields'));
-            expect(token).toBeUndefined();
-            done();
-        });
-    };
-
-    it('should throw an error if "iss" field is missing from claims', function(done) {
-        checkThatErrorIsReturnedWhenClaimsHasNoIssField('generateToken', done);
-        checkThatErrorIsReturnedWhenClaimsHasNoIssField('generateAuthorizationHeader', done);
-    });
-
-    var checkThatErrorIsReturnedWhenClaimsHasNoSubField = function(functionToTest, done) {
-        validator[functionToTest]({iss: 'iss'}, {publicKey: 'key'}, function(error, token) {
-            expect(jsonWebToken.create).not.toHaveBeenCalled();
-            expect(error).toEqual(new Error('claims body must have both the "iss" and "sub" fields'));
-            expect(token).toBeUndefined();
-            done();
-        });
-    };
-
-    it('should throw an error if "sub" field is missing from claims', function(done) {
-        checkThatErrorIsReturnedWhenClaimsHasNoSubField('generateToken', done);
-        checkThatErrorIsReturnedWhenClaimsHasNoSubField('generateAuthorizationHeader', done);
-    });
-
-    it('should pass the given token to decode', function (done) {
-        validator.validate('json-web-token', function () {
-            expect(jsonWebToken.decode).toHaveBeenCalledWith('json-web-token');
-            done();
-        });
-    });
-
-    it('should fetch the public key for the token', function (done) {
-        jsonWebToken.decode.andReturn({iss: 'an-issuer'});
-
-        validator.validate('json-web-token', function () {
-            expect(request).toHaveBeenCalledWith('http://a-public-key-server/an-issuer/public.pem');
-            done();
-        });
-    });
-
-    it('should verify the token using fetched public key', function (done) {
-        request.andReturn(q('public-key'));
-
-        validator.validate('json-web-token', function () {
-            expect(jsonWebToken.verify).toHaveBeenCalledWith('json-web-token', 'public-key');
-            done();
-        });
-    });
-
-    it('should return the verified claims', function (done) {
-        jsonWebToken.verify.andReturn(q({iss: 'an-issuer'}));
-
-        validator.validate('json-web-token', function (error, claims) {
-            expect(error).toBeNull('error');
-            expect(claims).toEqual({iss: 'an-issuer'}, 'claims');
-            done();
-        });
-    });
-
-    it('should return the error when decoding the claims fails', function (done) {
-        jsonWebToken.decode.andThrow(new Error('decode failed'));
-
-        validator.validate('json-web-token', function (error, claims) {
-            expect(error).toBeDefined('error');
-            expect(error.message).toBe('decode failed', 'error.message');
-            expect(claims).toBeUndefined('claims');
-            done();
-        });
-    });
-
-    it('should return an error when the claims section has no "iss" field', function (done) {
-        jsonWebToken.decode.andReturn({aToken: 'with-no-iss-field'});
-
-        validator.validate('json-web-token', function (error, claims) {
-            expect(error).toBeDefined('error');
-            expect(error.message).toBe('Cannot verify token with no "iss" claim', 'error.message');
-            expect(claims).toBeUndefined('claims');
-            done();
-        });
-    });
-
-    it('should return the error when fetching the public key fails', function (done) {
-        request.andReturn(q.reject(new Error('request failed')));
-
-        validator.validate('json-web-token', function (error, claims) {
-            expect(error).toBeDefined('error');
-            expect(error.message).toBe('request failed');
-            expect(claims).toBeUndefined('claims');
-            done();
-        });
-    });
-
-    it('should return the error when verifying the token fails', function (done) {
-        jsonWebToken.verify.andReturn(q.reject(new Error('token verification failed')));
-
-        validator.validate('json-web-token', function (error, claims) {
-            expect(error).toBeDefined('error');
-            expect(error.message).toBe('token verification failed');
-            expect(claims).toBeUndefined('claims');
-            done();
-        });
-    });
-
-    it('should not invoke the callback with errors thrown from the callback', function () {
-        var callback = jasmine.createSpy('callback').andThrow(new Error('oh no!'));
-        validator.validate('json-web-token', callback);
-
-        waitsFor(function () {
-            return callback.callCount > 0;
+            validator.generateToken({iss: 'iss', sub: 'sub'}, {privateKey: 'key'}, function (error, token) {
+                expect(error).toBeNull();
+                expect(token).toBe('token');
+                done();
+            });
         });
 
-        runs(function () {
-            expect(callback.callCount).toBe(1);
+        itShouldGenerateAToken('generateToken');
+    });
+
+    describe('generateAuthorizationHeader', function () {
+        it('should return the authorization header containing the token', function (done) {
+            jsonWebToken.create.andReturn('token');
+
+            var claims = {iss: 'iss', sub: 'sub'};
+            var options = {privateKey: 'key'};
+            validator.generateAuthorizationHeader(claims, options, function (error, headerValue) {
+                expect(error).toBeNull();
+                expect(headerValue).toBe('x-atl-jwt token');
+                done();
+            });
+        });
+
+        itShouldGenerateAToken('generateAuthorizationHeader');
+    });
+
+    describe('validate', function () {
+        it('should pass the given token to decode', function (done) {
+            validator.validate('json-web-token', function () {
+                expect(jsonWebToken.decode).toHaveBeenCalledWith('json-web-token');
+                done();
+            });
+        });
+
+        it('should fetch the public key for the token', function (done) {
+            jsonWebToken.decode.andReturn({iss: 'an-issuer'});
+
+            validator.validate('json-web-token', function () {
+                expect(request).toHaveBeenCalledWith('http://a-public-key-server/an-issuer/public.pem');
+                done();
+            });
+        });
+
+        it('should verify the token using fetched public key', function (done) {
+            request.andReturn(q('public-key'));
+
+            validator.validate('json-web-token', function () {
+                expect(jsonWebToken.verify).toHaveBeenCalledWith('json-web-token', 'public-key');
+                done();
+            });
+        });
+
+        it('should return the verified claims', function (done) {
+            jsonWebToken.verify.andReturn(q({iss: 'an-issuer'}));
+
+            validator.validate('json-web-token', function (error, claims) {
+                expect(error).toBeNull('error');
+                expect(claims).toEqual({iss: 'an-issuer'}, 'claims');
+                done();
+            });
+        });
+
+        it('should return the error when decoding the claims fails', function (done) {
+            jsonWebToken.decode.andThrow(new Error('decode failed'));
+
+            validator.validate('json-web-token', function (error, claims) {
+                expect(error).toBeDefined('error');
+                expect(error.message).toBe('decode failed', 'error.message');
+                expect(claims).toBeUndefined('claims');
+                done();
+            });
+        });
+
+        it('should return an error when the claims section has no "iss" field', function (done) {
+            jsonWebToken.decode.andReturn({aToken: 'with-no-iss-field'});
+
+            validator.validate('json-web-token', function (error, claims) {
+                expect(error).toBeDefined('error');
+                expect(error.message).toBe('Cannot verify token with no "iss" claim', 'error.message');
+                expect(claims).toBeUndefined('claims');
+                done();
+            });
+        });
+
+        it('should return the error when fetching the public key fails', function (done) {
+            request.andReturn(q.reject(new Error('request failed')));
+
+            validator.validate('json-web-token', function (error, claims) {
+                expect(error).toBeDefined('error');
+                expect(error.message).toBe('request failed');
+                expect(claims).toBeUndefined('claims');
+                done();
+            });
+        });
+
+        it('should return the error when verifying the token fails', function (done) {
+            jsonWebToken.verify.andReturn(q.reject(new Error('token verification failed')));
+
+            validator.validate('json-web-token', function (error, claims) {
+                expect(error).toBeDefined('error');
+                expect(error.message).toBe('token verification failed');
+                expect(claims).toBeUndefined('claims');
+                done();
+            });
+        });
+
+        it('should not invoke the callback with errors thrown from the callback', function () {
+            var callback = jasmine.createSpy('callback').andThrow(new Error('oh no!'));
+            validator.validate('json-web-token', callback);
+
+            waitsFor(function () {
+                return callback.callCount > 0;
+            });
+
+            runs(function () {
+                expect(callback.callCount).toBe(1);
+            });
         });
     });
 });
