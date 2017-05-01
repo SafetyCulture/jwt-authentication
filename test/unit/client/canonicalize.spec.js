705e1d52d8831120d8d1f9378130a8e0dc1fd3ea
Dipanjan Laha <dlaha@atlassian.com>
2017-05-01 15:44:25 +1000

    feat: throw error for malformed data uri

--- test/unit/client/canonicalize.spec.js
+++ test/unit/client/canonicalize.spec.js
@@ -19,9 +19,17 @@ describe('canonicalizePrivateKey', function () {
         expect(fn).toThrowError(/key id does not match the one included in data uri/);
     });
 
-    it('returns the private key if it does not match the pattern', function () {
+    it('returns the private key if it does not start with the `data:`', function () {
         var privateKey = canonicalizePrivateKey('an-issuer/the-keyid', keySupport.privateKeyPem);
 
         expect(privateKey).toBe(keySupport.privateKeyPem);
+    });
+
+    it('fails if the dats uri is malformed', function () {
+        var fn = function () {
+            canonicalizePrivateKey('an-issuer/the-keyid-2', 'data:application/pkcs8;kid;0BAQEFAASCAmIwggJeAgEAAoGBAM');
+        };
+
+        expect(fn).toThrowError(/Malformed Data URI/);
     });
 });
